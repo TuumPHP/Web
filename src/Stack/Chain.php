badc3212d439b99cc9886387a775aa4936761fa7
Asao <kamei@workspot.jp>
2015-01-11 13:37:19 +0900

    Chain stack for executing as a chain inside a chain.

---
+++ src/Stack/Chain.php
@@ -0,0 +1,40 @@
+<?php
+namespace Tuum\Web\Stack;
+
+use Tuum\Web\App\AppHandleInterface;
+use Tuum\Web\Http\Request;
+use Tuum\Web\Http\Response;
+
+class Chain extends Stackable
+{
+    /**
+     * @var AppHandleInterface[]
+     */
+    protected $apps = [];
+
+    /**
+     * @param AppHandleInterface $app
+     * @return $this
+     */
+    public function addApp($app)
+    {
+        $apps = func_get_args();
+        $this->apps = array_merge($this->apps, $apps);
+        return $this;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response|null
+     */
+    public function execute($request)
+    {
+        foreach($this->apps as $app) {
+            $response = $app->handle($request);
+            if($response) {
+                return $response;
+            }
+        }
+        return null;
+    }
+}
