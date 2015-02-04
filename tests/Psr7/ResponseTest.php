57157e39f161f44405b694e5f505c0ab0f5a716f
Asao <kamei@workspot.jp>
2015-02-04 14:39:51 +0900

    testing Response class.

---
+++ tests/Psr7/ResponseTest.php
@@ -0,0 +1,75 @@
+<?php
+namespace tests\Psr7;
+
+use Tuum\Web\Psr7\Respond;
+use Tuum\Web\Psr7\Response;
+
+class ResponseTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Respond
+     */
+    protected $respond;
+
+    function setup()
+    {
+        $this->respond = new Respond();
+    }
+    
+    function test0()
+    {
+        $response = $this->respond->asHtml('test');
+        $this->assertEquals('Tuum\Web\Psr7\Respond',  get_class($this->respond));
+        $this->assertEquals('Tuum\Web\Psr7\Response', get_class($response));
+    }
+
+    /**
+     * @test
+     */
+    function with_data()
+    {
+        $res1 = $this->respond->asHtml('test');
+        $this->assertEquals( null, $res1->getLocation() );
+
+        $res2 = $res1->withData( 'test', 'tested');
+        $this->assertNotSame($res1, $res2);
+        
+        $data = $res2->getData('test');
+        $this->assertEquals('tested', $data);
+
+        $data = $res2->getData();
+        $this->assertEquals(['test' => 'tested'], $data);
+
+        $res2 = $res2->withData( ['more' => 'done' ]);
+        $data = $res2->getData();
+        $this->assertEquals([
+            'test' => 'tested',
+            'more' => 'done',
+        ], $data);
+    }
+
+    /**
+     * @test
+     */
+    function location_redirect()
+    {
+        $res = $this->respond->asRedirectUri('/test');
+        $this->assertEquals( '/test', $res->getLocation() );
+        $this->assertTrue($res->isType(Response::TYPE_REDIRECT));
+    }
+
+    /**
+     * @test
+     */
+    function error_response()
+    {
+        $res = $this->respond->asError();
+        $this->assertEquals( null, $res->getLocation() );
+        $this->assertTrue($res->isType(Response::TYPE_ERROR));
+        $this->assertEquals('500', $res->getStatusCode());
+
+        $res = $this->respond->asNotFound();
+        $this->assertTrue($res->isType(Response::TYPE_ERROR));
+        $this->assertEquals('404', $res->getStatusCode());
+    }
+}
