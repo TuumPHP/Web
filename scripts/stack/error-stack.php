f4196d7c60a5a12d7ba76378ca6954bc8c518443
Asao <kamei@workspot.jp>
2015-02-05 17:42:20 +0900

    structured scripts as stack/service.

--- scripts/stack/error-stack.php
+++ scripts/stack/error-stack.php
@@ -5,7 +5,7 @@
 use Tuum\Web\App;
 use Tuum\Web\Web;
 
-$engine = $app->get('error-renderer-service');
+$engine = $app->get('service/error-renderer');
 $stack = new \Tuum\Web\Stack\ErrorStack($engine, $app->get(App::DEBUG));
 $stack->setLogger($app->get(App::LOGGER));
 
--- scripts/stack/session-stack.php
+++ scripts/stack/session-stack.php
@@ -1,12 +1,7 @@
 <?php
 
-/*
- * for now. until session stack is implemented
- */
-
 use Symfony\Component\HttpFoundation\Session\Session;
 use Tuum\Web\Stack\SessionStack;
-use Tuum\Web\App;
 
 /*
  * sample session stack constructor script for locator.
