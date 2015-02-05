1d81554a0b0403b349c694f2fe707ac128631fc5
Asao <kamei@workspot.jp>
2015-02-05 16:59:25 +0900

    removed unused constants.

--- src/App.php
+++ src/App.php
@@ -15,21 +15,24 @@ class App
     /*
      * directories
      */
-    const ROUTES_FILE  = 'file.routes';
     const CONFIG_DIR   = 'dir.config';
     const TEMPLATE_DIR = 'dir.view';
     const DOCUMENT_DIR = 'dir.resource';
     const VAR_DATA_DIR = 'dir.variable';
 
+    /*
+     * values
+     */
     const DEBUG = 'debug';
+    const TOKEN_NAME = '_token';
+
+    /*
+     * services
+     */
     const LOGGER = 'logger';
-    const VIEW_DATA = 'data';
-    const FLASH_NAME = 'flash';
     const ROUTE_NAMES = 'namedRoutes';
     const RENDER_ENGINE = 'renderer';
     const ROUTER = 'router';
-
     const SESSION_MGR = 'session.manager';
-    const TOKEN_NAME = '_token';
 
 }
